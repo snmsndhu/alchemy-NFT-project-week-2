6fd942f2831a4d1244c68fcd6b317d32a557bb7c
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-02 12:34:36 -0500

    contract

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -6,3 +6,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+
+contract ChainBattles is ERC721URIStorage {
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    mapping(uint256 => uint256) public tokenIdtoLevels;
+
+    constructor() ERC721("Chain Battles", "CBTLS"){
+        
+    }
+
+
+
+}
