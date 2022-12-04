3c6211b9465e33ce2cfffa84cab775cf1d2f0863
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2022-12-03 23:37:32 -0500

    done

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -18,21 +18,28 @@ contract ChainBattles is ERC721URIStorage {
 
     }
 
-    function generateCharacter(uint256 tokenId) public returns (string memory){
-        bytes memory svg = abi.encodePacked(
-           '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+    function generateCharacter(uint256 tokenId) public returns(string memory){
+
+    bytes memory svg = abi.encodePacked(
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
         '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
         '<rect width="100%" height="100%" fill="black" />',
         '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"Warrior",'</text>',
         '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
-        '</svg>');
-            return string(
+        '</svg>'
+    );
+    return string(
         abi.encodePacked(
             "data:image/svg+xml;base64,",
             Base64.encode(svg)
         )    
     );
-    }
+}
+
+    function getLevels(uint256 tokenId) public view returns(string memory){
+        uint256 levels = tokenIdtoLevels[tokenId];
+        return levels.toString();
+     }
 
 
 
